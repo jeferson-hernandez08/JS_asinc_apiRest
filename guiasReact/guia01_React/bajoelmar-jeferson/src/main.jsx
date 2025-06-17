a8c31b3b8ba97d2e583a7649588f35f5b3845616
Jeferson <jefer.hernandez1@gmail.com>
2025-06-16 21:37:11 -0500

    Guia-01-react finalizada

--- guiasReact/guia01_React/bajoelmar-jeferson/src/main.jsx
+++ guiasReact/guia01_React/bajoelmar-jeferson/src/main.jsx
@@ -1,10 +1,59 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles.css'
+//import App from './App.jsx'
+import { animales } from './animales.js'
+
+function App () { // Convertir todo en un componente funcional
+  const titulo = "";
+  const mostrarFondo = true; // Definir la variable faltante
+  
+  const fondo = (
+    <img 
+      className='fondo'
+      alt='fondoOceano'
+      src='/imagenes/fondoOceano.png'
+    />
+  );
+  
+  const imagenes = [];
+  for (const animal in animales) {
+    imagenes.push(
+      <img 
+        key={animal}
+        className="animal"
+        alt={animal}
+        src={animales[animal].imagen}
+        ariaLabel={animal}
+        role="button"
+        onClick={mostrarHecho}
+      />
+    )
+  }
+  
+  function mostrarHecho(e) {
+    const animalSeleccionado = e.target.alt;
+    const animalInfo = animales[animalSeleccionado];
+    const indiceOpcion = Math.floor(Math.random() * animalInfo.hechos.length);
+    const hecho = animalInfo.hechos[indiceOpcion];
+    document.getElementById("hecho").innerHTML = hecho;
+  }
+
+  return (
+    <div>
+      <h1>{titulo === "" ? "Click Sobre un Animal" : titulo}</h1>
+      {mostrarFondo && fondo}
+      <p id='hecho'></p>
+      <div className="animales">
+        {imagenes}
+      </div>
+    </div>
+  );
+
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
